0a43053293dd4da0e8cee6eb19297f0867499f1f
Jason1412 <jasontrabajo1@gmail.com>
2019-10-12 19:57:10 +0200

    Added operation to storageService

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StorageService {
 
-  constructor() { }
+  constructor(
+    private storage: Storage
+  ) { }
+
+  set(key: string, value: any) {
+    return this.storage.set(key, value);
+  }
+
+  get(key: string) {
+    return this.storage.get(key);
+  }
 }
