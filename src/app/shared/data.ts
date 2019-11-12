d8ebe1f1b52c147b4b42d1f2b10ff2f62c8b6027
Jason1412 <jasontrabajo1@gmail.com>
2019-11-12 22:34:00 +0100

    Refactoring data

--- src/app/shared/data.ts
+++ src/app/shared/data.ts
@@ -5,18 +5,18 @@ export const NOTES: Note[] = [
         id: '0',
         title: 'My first Note',
         text: 'This is my first text',
-        date: new Date()
+        date: new Date('September 21, 2018 23:15:30')
     },
     {
         id: '1',
         title: 'My second Note',
         text: 'This is my second description',
-        date: new Date()
+        date: new Date('October 12, 2019 09:15:30')
     },
     {
         id: '2',
         title: 'My third Note',
         text: 'This is my third description',
-        date: new Date()
+        date: new Date('December 4, 2019 15:15:30')
     }
 ];
