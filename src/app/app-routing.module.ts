0927268e89437d25626f623a93a59f5396efc3fb
Jason1412 <jasontrabajo1@gmail.com>
2019-11-16 12:12:45 +0100

    Create user page and navigation method

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
   { path: 'note-detail', loadChildren: './note-detail/note-detail.module#NoteDetailPageModule' },
+  { path: 'user', loadChildren: () => import('./user/user.module').then( m => m.UserPageModule )}
 ];
 
 @NgModule({
