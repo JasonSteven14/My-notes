8bfcc1c8de9703685be8e5d31a526b6ec077f60b
Harut <harutyun.yer@gmail.com>
2020-01-24 01:13:20 +0100

    Added test suggestion observable

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,24 +1,58 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
 import { StorageService } from '../services/storage.service';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { Observable, fromEvent, Subscription, of } from 'rxjs';
+import { map, debounceTime, filter, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
+import { NoteService } from '../services/note.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage {
+export class LoginPage implements AfterViewInit {
+  @ViewChild('addressSearch', { static: false }) addressSearch: ElementRef;
 
   isLoginView = true;
   form: FormGroup;
+  loginForm = new FormGroup({
+    addressSearch: new FormControl('')
+  });
 
   user = '';
   pass = '';
 
+  addressSearchObservable: Observable<{ street: string, postalCode: string }[]>;
+
   constructor(
     private strgService: StorageService,
-    private fb: FormBuilder
+    private fb: FormBuilder,
+    private noteService: NoteService
   ) { }
+
+  ngAfterViewInit() {
+    console.log('setting up address search observable');
+    console.log('addressSearch input: ', this.loginForm.get('addressSearch'));
+    /*
+    this.addressSearchObservable = this.loginForm.get('addressSearch').valueChanges
+      .pipe(
+        debounceTime(250),
+        filter(query => query.length >= 2 || query.length === 0),
+        distinctUntilChanged(),
+        switchMap(value => {
+          console.log('calling suggestion service');
+          return value
+            ? this.noteService.getAddressSuggestion(value).pipe(catchError(_ => of([])))
+            : of([]);
+        })
+      );
+      */
+
+    this.addressSearchObservable = this.noteService.getInputSuggestions(
+      this.loginForm.get('addressSearch').valueChanges,
+      this.noteService.getAddressSuggestion
+    );
+  }
 
   setViewLogin() {
     this.isLoginView = true;
--- src/app/services/note.service.ts
+++ src/app/services/note.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@angular/core';
 import { NOTES } from '../shared/data';
-import { of, Observable, throwError } from 'rxjs';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import { delay } from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { delay, debounceTime, filter, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
 import { Note } from '../shared/note';
-import { AngularFireAuth } from '@angular/fire/auth';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +11,9 @@ import { AngularFireAuth } from '@angular/fire/auth';
 
 export class NoteService {
 
-  constructor(private http: HttpClient,
-      ) { }
+  constructor(
+    private http: HttpClient,
+  ) { }
 
   getNotes() {
     // return this.http.get('../shared/data');
@@ -28,4 +28,33 @@ export class NoteService {
     NOTES.push(note);
   }
 
+  getAddressSuggestion(streetName: string) {
+    const addressList: { street: string, postalCode: string }[] = [{
+      street: 'Miniaturista Meseguer 21, Benimámet, Valencia, 46035',
+      postalCode: '46035'
+    }, {
+      street: 'Felipe Valls 53, Benimámet, Valencia, 46035',
+      postalCode: '46035'
+    }, {
+      street: 'Martí 16, Valencia, Valencia, 46005',
+      postalCode: '46005'
+    }];
+    const list = addressList.filter(address => address.street.includes(streetName));
+    console.log('Address suggestion: ', list);
+    return of(list);
+    // return throwError('ERROR');
+  }
+
+  getInputSuggestions(valueChanges: Observable<string>, fetchFunction: (query: string) => Observable<any>) {
+    return valueChanges.pipe(
+      debounceTime(250),
+      filter(query => query.length >= 2 || query.length === 0),
+      distinctUntilChanged(),
+      switchMap(value => {
+        return value
+          ? fetchFunction(value).pipe(catchError(_ => of([])))
+          : of([]);
+      })
+    );
+  }
 }
