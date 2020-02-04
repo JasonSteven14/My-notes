c096be8077c4bdb2a14f04992729e5c124cc2e75
JasonSteven14 <jasontrabajo1@gmail.com>
2020-02-04 22:14:31 +0100

    Change of perspective

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,76 +1,118 @@
-import { Component, ElementRef, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
-import { StorageService } from '../services/storage.service';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
-import { Observable, fromEvent, Subscription, of } from 'rxjs';
-import { map, debounceTime, filter, distinctUntilChanged, switchMap, catchError } from 'rxjs/operators';
-import { NoteService } from '../services/note.service';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../services/authentication.service';
+import { IfStmt } from '@angular/compiler';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements AfterViewInit {
-  @ViewChild('addressSearch', { static: false }) addressSearch: ElementRef;
-
+export class LoginPage {
   isLoginView = true;
-  form: FormGroup;
-  loginForm = new FormGroup({
-    addressSearch: new FormControl('')
-  });
+  errorMessage = '';
+  logInForm: FormGroup;
+  signUpForm: FormGroup;
 
-  user = '';
-  pass = '';
+  formErrors = {
+    Firstname: '',
+    Lastname: '',
+    Email: '',
+    Password: '',
+    emailLogInput: '',
+    passwordLogInput: ''
+  };
 
-  addressSearchObservable: Observable<{ street: string, postalCode: string }[]>;
+  validationMessages = {
+    Firstname: {
+      required: 'First Name is required.',
+      minlength: 'First Name must be at least 2 characters long.',
+      maxlength: 'First Name cannot be more than 25 characters long.'
+    },
+    Lastname: {
+      required: 'Last Name is required.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.'
+    },
+    Email: {
+      required: 'Email is required.',
+      pattern: 'Email not in valid format.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.'
+    },
+    Password: {
+      required: 'Password required',
+      minlength: 'Password must be at least 2 characters long.',
+      maxlength: 'Password cannot be more than 25 characters long.'
+    },
+    emailLogInput: {
+      required: 'Password required',
+      pattern: 'Email not in valid format.',
+      minlength: 'Last Name must be at least 2 characters long.',
+      maxlength: 'Last Name cannot be more than 25 characters long.'
+    },
+    passwordLogInput: {
+      required: 'Password required',
+      minlength: 'Password must be at least 2 characters long.',
+      maxlength: 'Password cannot be more than 25 characters long.'
+    }
+  };
 
   constructor(
-    private strgService: StorageService,
-    private fb: FormBuilder,
-    private noteService: NoteService
-  ) { }
+    private authservice: AuthenticationService
+  ) {
+    this.createForm();
+  }
 
-  ngAfterViewInit() {
-    console.log('setting up address search observable');
-    console.log('addressSearch input: ', this.loginForm.get('addressSearch'));
-    /*
-    this.addressSearchObservable = this.loginForm.get('addressSearch').valueChanges
-      .pipe(
-        debounceTime(250),
-        filter(query => query.length >= 2 || query.length === 0),
-        distinctUntilChanged(),
-        switchMap(value => {
-          console.log('calling suggestion service');
-          return value
-            ? this.noteService.getAddressSuggestion(value).pipe(catchError(_ => of([])))
-            : of([]);
-        })
-      );
-      */
+  createForm() {
+    this.logInForm = new FormGroup({
+      emailLogInput: new FormControl('', [Validators.required, Validators.email, Validators.min(2), Validators.max(15)]),
+      passwordLogInput: new FormControl('', [Validators.required, Validators.min(2), Validators.max(15)])
+    });
 
-    this.addressSearchObservable = this.noteService.getInputSuggestions(
-      this.loginForm.get('addressSearch').valueChanges,
-      this.noteService.getAddressSuggestion
-    );
+    this.signUpForm = new FormGroup({
+      Firstname: new FormControl('', [Validators.required, Validators.min(2), Validators.max(15)]),
+      Lastname: new FormControl('', [Validators.required, Validators.min(2), Validators.max(15)]),
+      Email: new FormControl('', [Validators.required, Validators.email, Validators.min(2), Validators.max(15)]),
+      Password: new FormControl('', [Validators.required, Validators.min(2), Validators.max(15)])
+    });
+    this.logInForm.valueChanges.subscribe(data => this.onValueChanges(data));
+    this.signUpForm.valueChanges.subscribe(data => this.onValueChanges(data));
+  }
+
+  onValueChanges(data?: any) {
+    if (!data) { return; }
+    console.log('this is data 1: ', data);
+    const control = this.logInForm.value.emailLogInput;
+    // tslint:disable-next-line: forin
+    this.formErrors.emailLogInput = this.validationMessages.emailLogInput.required;
+    console.log(this.formErrors);
   }
 
   setViewLogin() {
     this.isLoginView = true;
+    this.signUpForm.reset();
   }
 
   setViewSignup() {
     this.isLoginView = false;
+    this.logInForm.reset();
   }
 
-  createForm() {
-    this.form = this.fb.group({
-      userName: '',
-      userPass: ''
+  onSubmitLoginButton() {
+    console.log(this.logInForm.value);
+    const email = this.logInForm.value.emailLogInput;
+    const pass = this.logInForm.value.passwordLogInput;
+    this.authservice.login(email, pass).then((data) => { console.log(data); });
+  }
+
+  onSubmitSignupButton() {
+    const email = this.signUpForm.value.Email;
+    const pass = this.signUpForm.value.Password;
+    this.authservice.signup(email, pass).then(res => {
+      console.log('User created: ', res);
+    }).catch(err => {
+      console.log('catch pagina: ', err);
     });
   }
-
-  getUser() {
-    this.strgService.getUser(this.user, this.pass);
-  }
-
 }
